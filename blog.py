bb7d8a2d67ec994f7911e84afa1d48c15ce0428b
cmdada <dotfiles@adas.software>
2024-03-01 09:43:52 -0700

    added better header support

--- blog.py
+++ blog.py
@@ -13,14 +13,13 @@ with open(template_file, 'r') as file:
 template = Template(template_html)
 
 # Read and convert Markdown files
-# Read and convert Markdown files
 markdown_files = [f for f in os.listdir(markdown_dir) if f.endswith('.md')]
 markdown_content = []
 
 for md_file in markdown_files:
     with open(os.path.join(markdown_dir, md_file), 'r') as file:
         md_content = file.read()
-        html_content = markdown.markdown(md_content)
+        html_content = markdown.markdown(md_content, extensions=['markdown.extensions.toc', 'markdown.extensions.extra'])
         markdown_content.append({'content': html_content})  # Store title and content
 
 # Combine Markdown content with the HTML template
